33daadd328f124bbfaa38b062bec9853b3978100
Manmeet2018 <manietdavv@gmail.com>
2021-04-12 13:29:28 +0530

    feat: Add More negative testCase

--- test/ERC20.js
+++ test/ERC20.js
@@ -1,4 +1,8 @@
 let ERC20 = artifacts.require("ERC20"); // contractName
+const {
+  constants, // Common constants, like the zero address and largest integers
+  expectRevert, // Assertions for transactions that should fail
+} = require("@openzeppelin/test-helpers");
 
 const chai = require("chai");
 chai.should();
@@ -55,39 +59,78 @@ contract("ERC20", (accounts) => {
 
   describe("transfer", () => {
     it("Parent transfer 500 tokens to Child A", async () => {
-      await this.token.transfer(accounts[1], 500, {
-        from: accounts[0],
+      const result = await this.token.transfer(spender, 500, {
+        from: tokenOwner,
       });
-      const balanceOfParent = await this.token.balanceOf(accounts[0]);
-      const balanceOfChildA = await this.token.balanceOf(accounts[1]);
-
-      assert.equal(balanceOfChildA, 500);
-      assert.equal(balanceOfParent, 500);
+      const log = result.logs[0];
+      assert.equal(log.event, "Transfer");
+      assert.equal(log.args.from, tokenOwner);
+      assert.equal(log.args.to, spender);
+    });
+    it("reverts when transferring tokens to the zero address", async () => {
+      await expectRevert(
+        this.token.transfer(constants.ZERO_ADDRESS, 0, {
+          from: tokenOwner,
+        }),
+        "ERC20: Can't transfer from the zero address"
+      );
     });
   });
+
   describe("transfer from", () => {
     describe("when the spender has enough approved balance", () => {
       beforeEach(async () => {
         await this.token.approve(spender, approvalAmt, { from: tokenOwner });
       });
       describe("when the token owner has enough balance", () => {
-        it("transfers the requested amount", async () => {
-          const tokenOwnerAmtBefore = await this.token.balanceOf(accounts[0]);
-          await this.token.transferFrom(
+        it("transfers the requested amount which is less than or equal to the approval amount", async () => {
+          const result = await this.token.transferFrom(
             tokenOwner,
             recipient,
             transferFromAmt,
             { from: spender }
           );
-
-          const tokenOwnerAmt = await this.token.balanceOf(accounts[0]);
-          const recipientAmt = await this.token.balanceOf(accounts[2]);
-          const ownerAmtTransferred = tokenOwnerAmtBefore - recipientAmt;
-          assert.equal(tokenOwnerAmt, ownerAmtTransferred);
-          assert.equal(recipientAmt, transferFromAmt);
+          const log = result.logs[0];
+          assert.equal(log.event, "Transfer");
+          assert.equal(log.args.from, tokenOwner);
+          assert.equal(log.args.to, recipient);
+        });
+        it("transfers the requested amount which is more than or equal to the approval amount", async () => {
+          await expectRevert(
+            this.token.transferFrom(tokenOwner, recipient, initialSupply, {
+              from: spender,
+            }),
+            "ERC20: Transfer amount exceeds allowance"
+          );
+        });
+        it("transfers the requested amount which is more than or equal to the approval amount", async () => {
+          await expectRevert(
+            this.token.transferFrom(
+              constants.ZERO_ADDRESS,
+              recipient,
+              initialSupply,
+              {
+                from: spender,
+              }
+            ),
+            "ERC20: Can't transfer from the zero address"
+          );
+        });
+        it("transfers the requested amount which is more than or equal to the approval amount", async () => {
+          await expectRevert(
+            this.token.transferFrom(
+              tokenOwner,
+              constants.ZERO_ADDRESS,
+              initialSupply,
+              {
+                from: spender,
+              }
+            ),
+            "ERC20: Can't transfer from the zero address"
+          );
         });
       });
-      describe("when there was approved amount before", () => {
+      describe("Approval", () => {
         it("check approval amount", async () => {
           const approvalAmtByOwner = await this.token.allowance(
             tokenOwner,
@@ -96,6 +139,17 @@ contract("ERC20", (accounts) => {
           assert.equal(approvalAmtByOwner, approvalAmt);
         });
       });
+    });
+  });
+
+  describe("Mint", () => {
+    it("Mint to Zero address", async () => {
+      await expectRevert(
+        this.token.mint(constants.ZERO_ADDRESS, 500, {
+          from: tokenOwner,
+        }),
+        "ERC20: mint to the zero address"
+      );
     });
   });
 });
